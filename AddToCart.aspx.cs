61132e50d95898ad5df43da86a0ebc466ef82d99
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-04-07 13:20:04 -0400

    update

--- AddToCart.aspx.cs
+++ AddToCart.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Diagnostics;
+using WingtipToys.Logic;
 
 namespace WingtipToys
 {
@@ -11,7 +13,22 @@ namespace WingtipToys
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string rawId = Request.QueryString["ProductID"];
+            int productId;
+            if (!String.IsNullOrEmpty(rawId) && int.TryParse(rawId, out productId))
+            {
+                using (ShoppingCartActions usersShoppingCart = new ShoppingCartActions())
+                {
+                    usersShoppingCart.AddToCart(Convert.ToInt16(rawId));
+                }
 
+            }
+            else
+            {
+                Debug.Fail("ERROR : We should never get to AddToCart.aspx without a ProductId.");
+                throw new Exception("ERROR : It is illegal to load AddToCart.aspx without setting a ProductId.");
+            }
+            Response.Redirect("ShoppingCart.aspx");
         }
     }
 }
